5c57991238b389644010a940f00279ccdff72fc1
Guzman Zugnoni <gooseiman@protonmail.com>
2022-08-18 18:29:45 -0300

    Init

---
+++ src/frontend/MainMenu.java
@@ -0,0 +1,20 @@
+package frontend;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+/**
+ *
+ * @author chonk
+ */
+public class MainMenu extends JFrame implements Runnable {
+    
+    @Override
+    public void run() {
+        this.init();
+    }
+
+    public void init() {
+		// TODO: Implement the window
+    }
+}
