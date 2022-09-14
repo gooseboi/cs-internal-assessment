a07de28049e4e51ef2441c1a02dfdc81a0ca65a8
Guzman Zugnoni <gooseiman@protonmail.com>
2022-09-14 10:32:35 -0300

    Start implementin the backend

---
+++ src/backend/BuyOrderList.java
@@ -0,0 +1,101 @@
+/*
+Copyright (c) 2022 Guzman Zugnoni <gooseiman@protonmail.com>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+ */
+package backend;
+
+import java.util.Date;
+
+/**
+ *
+ * @author chonk
+ */
+public class BuyOrderList {
+
+    public BuyOrderNode first;
+
+    public BuyOrderList getOrdersByClient(Client c) {
+        BuyOrderList ret = new BuyOrderList();
+        int top = 0;
+        BuyOrderNode aux = first;
+        while (aux != null) {
+            if (aux.data.client.equals(c)) {
+                ret.insert(aux.data);
+            }
+            aux = aux.next;
+        }
+        return ret;
+    }
+
+    public BuyOrderList getOrdersByDate(Date date) {
+        return this.getOrdersByDateRange(date, date);
+    }
+
+    public BuyOrderList getOrdersByDateRange(Date start, Date end) {
+        BuyOrderList ret = new BuyOrderList();
+        BuyOrderNode aux = first;
+        while (aux != null) {
+            Date date = aux.data.date;
+            if (date.before(end) && date.after(start)) {
+                ret.insert(aux.data);
+            }
+            aux = aux.next;
+        }
+        return ret;
+    }
+
+    public BuyOrderList getOrdersByPlant(Plant p) {
+        BuyOrderList ret = new BuyOrderList();
+        BuyOrderNode aux = first;
+        while (aux != null) {
+            if (aux.data.orders.contains(p)) {
+                ret.insert(aux.data);
+            }
+            aux = aux.next;
+        }
+        return ret;
+    }
+
+    public int size() {
+        BuyOrderNode aux = first;
+        int counter = 0;
+        while (aux != null) {
+            counter++;
+            aux = aux.next;
+        }
+        return counter;
+    }
+
+    public boolean insert(BuyOrder order) {
+        if (first == null) {
+            first = new BuyOrderNode(order);
+            return true;
+        }
+
+        BuyOrderNode aux = first;
+        while (aux.next != null) {
+            if (aux.data.equals(order))
+                return false;
+            aux = aux.next;
+        }
+        aux.next = new BuyOrderNode(order);
+        return true;
+    }
+}
