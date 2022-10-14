d5f63dcbcbc33ae0309c66af30ab4b6459376523
Guzman Zugnoni <gooseiman@protonmail.com>
2022-10-14 08:21:17 -0300

    Make EditOrder a single window that works like a shopping cart, and remove AddOrder

--- src/frontend/EditOrder.java
+++ src/frontend/EditOrder.java
@@ -26,6 +26,11 @@ import backend.OrderList;
 import javax.swing.event.TableModelEvent;
 import javax.swing.event.TableModelListener;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.DefaultListModel;
+import static backend.Main.stocks;
+import backend.Order;
+import backend.Plant;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -42,11 +47,16 @@ public class EditOrder extends javax.swing.JPanel {
      */
     public EditOrder(JFrame window, OrderList orders, String parent) {
         initComponents();
+        ordersTable.setRowSelectionAllowed(false);
+
         this.window = window;
         this.orders = orders;
         this.parent = parent;
 
         this.drawTable();
+        DefaultListModel<String> model = new DefaultListModel();
+        stockList.setModel(model);
+        this.drawList();
     }
 
     private void drawTable() {
@@ -65,6 +75,21 @@ public class EditOrder extends javax.swing.JPanel {
         ordersTable.setModel(model);
     }
 
+    private void drawList() {
+        var node = stocks.getFirst();
+        DefaultListModel<String> model = (DefaultListModel<String>) stockList.getModel();
+        model.clear();
+        while (node != null) {
+            var p = node.getData().getPlant();
+            if (!orders.contains(p)) {
+                model.addElement(p.getName());
+            }
+            node = node.getNext();
+        }
+
+        stockList.setModel(model);
+    }
+
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -80,6 +105,10 @@ public class EditOrder extends javax.swing.JPanel {
         ordersTable = new javax.swing.JTable();
         titleLabel = new javax.swing.JLabel();
         deleteButton = new javax.swing.JButton();
+        amountSpinner = new javax.swing.JSpinner();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        stockList = new javax.swing.JList<>();
+        jTextField1 = new javax.swing.JTextField();
 
         backButton.setText("Back");
         backButton.addActionListener(new java.awt.event.ActionListener() {
@@ -88,7 +117,7 @@ public class EditOrder extends javax.swing.JPanel {
             }
         });
 
-        addButton.setText("Add");
+        addButton.setText(">>");
         addButton.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 addButtonActionPerformed(evt);
@@ -105,7 +134,15 @@ public class EditOrder extends javax.swing.JPanel {
             new String [] {
                 "Number Ordered", "Plant Name"
             }
-        ));
+        ) {
+            boolean[] canEdit = new boolean [] {
+                true, false
+            };
+
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit [columnIndex];
+            }
+        });
         ordersTable.getModel().addTableModelListener(
             new TableModelListener()
             {
@@ -125,50 +162,95 @@ public class EditOrder extends javax.swing.JPanel {
             titleLabel.setText("Edit Orders");
             titleLabel.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
 
-            deleteButton.setText("Delete");
+            deleteButton.setText("<<");
+            deleteButton.addActionListener(new java.awt.event.ActionListener() {
+                public void actionPerformed(java.awt.event.ActionEvent evt) {
+                    deleteButtonActionPerformed(evt);
+                }
+            });
+
+            stockList.setModel(new javax.swing.AbstractListModel<String>() {
+                String[] strings = { "Item 1", "Item 2", "Item 3", "Item 4", "Item 5" };
+                public int getSize() { return strings.length; }
+                public String getElementAt(int i) { return strings[i]; }
+            });
+            stockList.setSelectionMode(javax.swing.ListSelectionModel.SINGLE_SELECTION);
+            jScrollPane2.setViewportView(stockList);
+
+            jTextField1.setHorizontalAlignment(javax.swing.JTextField.CENTER);
+            jTextField1.setText("Order Amount");
 
             javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
             this.setLayout(layout);
             layout.setHorizontalGroup(
                 layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                 .addGroup(layout.createSequentialGroup()
+                    .addGap(40, 40, 40)
+                    .addComponent(backButton, javax.swing.GroupLayout.PREFERRED_SIZE, 70, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                    .addContainerGap(130, Short.MAX_VALUE)
                     .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                        .addGroup(layout.createSequentialGroup()
-                            .addGap(259, 259, 259)
-                            .addComponent(titleLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 171, javax.swing.GroupLayout.PREFERRED_SIZE))
-                        .addGroup(layout.createSequentialGroup()
-                            .addGap(159, 159, 159)
+                        .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                            .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 144, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                             .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                                .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                                    .addComponent(deleteButton, javax.swing.GroupLayout.PREFERRED_SIZE, 90, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                    .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                    .addComponent(addButton, javax.swing.GroupLayout.PREFERRED_SIZE, 90, javax.swing.GroupLayout.PREFERRED_SIZE))
-                                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 379, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                                .addComponent(addButton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addComponent(deleteButton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addComponent(jTextField1)
+                                .addComponent(amountSpinner, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 102, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                            .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 243, javax.swing.GroupLayout.PREFERRED_SIZE))
                         .addGroup(layout.createSequentialGroup()
-                            .addGap(40, 40, 40)
-                            .addComponent(backButton, javax.swing.GroupLayout.PREFERRED_SIZE, 70, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                    .addGap(0, 162, Short.MAX_VALUE))
+                            .addGap(118, 118, 118)
+                            .addComponent(titleLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 171, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 212, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                    .addGap(69, 69, 69))
             );
             layout.setVerticalGroup(
                 layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                 .addGroup(layout.createSequentialGroup()
-                    .addGap(42, 42, 42)
+                    .addGap(37, 37, 37)
                     .addComponent(titleLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 46, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 249, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addGap(18, 18, 18)
-                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                        .addComponent(addButton)
-                        .addComponent(deleteButton))
-                    .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 31, Short.MAX_VALUE)
+                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGroup(layout.createSequentialGroup()
+                            .addGap(93, 93, 93)
+                            .addComponent(addButton)
+                            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                            .addComponent(deleteButton)
+                            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                            .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                            .addComponent(amountSpinner, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGroup(layout.createSequentialGroup()
+                            .addGap(18, 18, 18)
+                            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                .addComponent(jScrollPane2)
+                                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 249, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                    .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 79, Short.MAX_VALUE)
                     .addComponent(backButton)
                     .addGap(40, 40, 40))
             );
         }// </editor-fold>//GEN-END:initComponents
 
     private void addButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_addButtonActionPerformed
-        this.window.setContentPane(new AddOrder(window, orders, parent));
-        this.window.pack();
+        String value = stockList.getSelectedValue();
+        int idx = stockList.getSelectedIndex();
+        if (value == null) {
+            JOptionPane.showMessageDialog(this, "No item selected!", "ERROR!", JOptionPane.ERROR_MESSAGE);
+            return;
+        }
+
+        int amount = (int) amountSpinner.getValue();
+        if (amount <= 0) {
+            JOptionPane.showMessageDialog(this, "Must add 1 or more of the item!", "ERROR!", JOptionPane.ERROR_MESSAGE);
+            return;
+        }
+
+        ((DefaultListModel<String>) stockList.getModel()).remove(idx);
+        Plant p = stocks.findPlant(value);
+        orders.insert(new Order(p, amount));
+        this.drawTable();
     }//GEN-LAST:event_addButtonActionPerformed
 
     private void backButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_backButtonActionPerformed
@@ -185,20 +267,43 @@ public class EditOrder extends javax.swing.JPanel {
             this.window.setContentPane(new ManageBuyOrders(window));
             this.window.pack();
         } else {
-            // TODO: Throw error and terminate
+            throw new UnsupportedOperationException("Invalid parent window in EditOrder");
         }
     }//GEN-LAST:event_backButtonActionPerformed
 
+    private void deleteButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_deleteButtonActionPerformed
+        int row = ordersTable.getSelectedRow();
+        if (row == -1) {
+            JOptionPane.showMessageDialog(this, "No item selected!", "ERROR!", JOptionPane.ERROR_MESSAGE);
+            return;
+        }
+
+        ((DefaultListModel<String>) stockList.getModel()).remove(idx);
+        Plant p = stocks.findPlant(value);
+        orders.insert(new Order(p, amount));
+        this.drawTable();
+    }//GEN-LAST:event_deleteButtonActionPerformed
+
     private void onCellChanged(TableModelEvent evt) {
-        System.out.println("Event fired");
+        if (evt.getType() == TableModelEvent.UPDATE) {
+            int col = evt.getColumn();
+            int row = evt.getFirstRow();
+            assert evt.getFirstRow() == evt.getLastRow() : "First row is not last row";
+            System.out.println("Col" + col);
+            System.out.println("Row" + row);
+        }
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton addButton;
+    private javax.swing.JSpinner amountSpinner;
     private javax.swing.JButton backButton;
     private javax.swing.JButton deleteButton;
     private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JTextField jTextField1;
     private javax.swing.JTable ordersTable;
+    private javax.swing.JList<String> stockList;
     private javax.swing.JLabel titleLabel;
     // End of variables declaration//GEN-END:variables
 }
